2b8070c86a0ca09eac555089cffe0b4476d867c8
varunk19 <rkoslam3@gmail.com>
2024-05-25 08:10:59 +0530

    Final env commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://backend-olive-eta-22.vercel.app' // Replace with your actual production base URL
+    baseUrl: 'https://varunj6v1k9.pythonanywhere.com/' // Replace with your actual production base URL
   };
